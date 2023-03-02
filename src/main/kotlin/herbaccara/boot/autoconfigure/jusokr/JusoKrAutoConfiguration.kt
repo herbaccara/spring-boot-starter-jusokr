682b787c6afbcc445d3c9c03d93f393b7b65b853
이용홍 <allbegray.unimewo@gmail.com>
2023-03-02 15:19:37 +0900

    :art: clean code

--- src/main/kotlin/herbaccara/boot/autoconfigure/jusokr/JusoKrAutoConfiguration.kt
+++ src/main/kotlin/herbaccara/boot/autoconfigure/jusokr/JusoKrAutoConfiguration.kt
@@ -7,12 +7,8 @@ import org.springframework.boot.autoconfigure.AutoConfiguration
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.boot.context.properties.EnableConfigurationProperties
-import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
-import org.springframework.http.converter.StringHttpMessageConverter
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
 import org.springframework.web.client.RestTemplate
-import java.nio.charset.StandardCharsets
 import java.util.*
 
 @AutoConfiguration
@@ -34,18 +30,11 @@ class JusoKrAutoConfiguration {
 
     @Bean
     fun dataKrService(
+        restTemplate : RestTemplate,
         objectMapper: ObjectMapper,
         properties: JusoKrProperties
     ): JusoKrService {
         if (properties.confmKey.isEmpty()) throw NullPointerException()
-
-        val restTemplate = RestTemplateBuilder()
-            .rootUri(properties.rootUri)
-            .messageConverters(
-                StringHttpMessageConverter(StandardCharsets.UTF_8),
-                MappingJackson2HttpMessageConverter(objectMapper)
-            )
-            .build()
 
         return JusoKrService(restTemplate, objectMapper, properties)
     }
